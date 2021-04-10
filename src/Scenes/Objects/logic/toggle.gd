7853d7f850c106d86b1c2e1a87dc3b0d0cb9590f
Immortalsonly <remco.eijsackers@gmail.com>
2021-04-10 13:00:01 +0200

    toggle block

---
+++ src/Scenes/Objects/logic/toggle.gd
@@ -0,0 +1,19 @@
+extends "BlockLogic.gd"
+
+#overide physics trigger 
+func  apply_central_impulse(_force):
+	pass
+	
+func on_empty_hit():
+	if $off.visible == true:
+		$on.set_visible(true)
+		#TODO implement toggle signal
+		$off.set_visible(false)
+	else:
+		$on.set_visible(false)
+		$off.set_visible(true)			
+
+# Break on buttjump
+func _on_Area2D_area_entered(area):
+	if area.get_parent().is_in_group("player"):
+		pass
